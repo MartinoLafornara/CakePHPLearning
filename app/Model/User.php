4d183c171e0c95c371cd6d70c28558d6121fe6a8
Martino Lafornara <lafornara.martino@gmail.com>
2018-03-28 09:39:45 +0200

    Validation User Variables

--- app/Model/User.php
+++ app/Model/User.php
@@ -6,24 +6,46 @@ App::uses('BlowfishPasswordHasher', 'Controller/Component/Auth');
 
 class User extends AppModel {
     public $validate = array(
-        'username' => array(
-            'required' => array(
-                'rule' => 'notEmpty',
-                'message' => 'Username richiesto'
-            )
+        'first_name' => array(
+            'rule' => 'alpha',
+            'message' => 'Solo caratteri alfabetici',
+            'allowEmpty' => false,
+            'required' => true
+        ),
+        'last_name' => array(
+            'rule' => 'alpha',
+            'message' => 'Solo caratteri alfabetici',
+            'allowEmpty' => false,
+            'required' => true
         ),
         'password' => array(
-            'required' => array(
-                'rule' => 'notEmpty',
-                'message' => 'Password richiesta'
-            )
+            'rule' => array('minLength',8),
+            'message' => 'La password deve essere lunga almeno 8 caratteri',
+            'required' => true
         ),
         'role' => array(
-            'valid' => array(
-                'rule' => array('inList', array('admin', 'author')),
-                'message' => 'Inserisci un ruolo valido',
-                'allowEmpty' => false
+            'rule' => array('inList', array('admin', 'author')),
+            'message' => 'Inserisci un ruolo valido',
+            'allowEmpty' => false,
+            'required' => true
+        ),
+        'email' => array(
+            'email1' => array(
+                'rule' => array('email', true),
+                'message' => "Inserisci un'email valida",
+                'allowEmpty' => false,
+                'required' => true
+            ),
+            'email2' => array(
+                'rule' => 'isUnique',
+                'message' => 'Questa email è già stata registrata'
             )
+        ),
+        'date_birth' => array(
+            'rule' => array('date', 'dmy'),
+            'message' => 'Inserisci una data valida',
+            'allowEmpty' => false,
+            'required' => true
         )
     );
 
@@ -35,6 +57,13 @@ class User extends AppModel {
     );
 
     public function beforeSave($options = array()) {
+        //Adatta il Date Format per il Database
+        if (!empty($this->data['User']['date_birth'])) {
+          //pr($this->data['User']['date_birth']); exit;
+          $this->data['User']['date_birth'] = $this->dateFormatBeforeSave($this->data['User']['date_birth']);
+          //pr($this->data['User']['date_birth']); exit;
+        }
+        //Cifra la password inserita
         if (isset($this->data[$this->alias]['password'])) {
             $passwordHasher = new BlowfishPasswordHasher();
             $this->data[$this->alias]['password'] = $passwordHasher->hash(
@@ -43,6 +72,17 @@ class User extends AppModel {
         }
         return true;
     }
+
+    public function dateFormatBeforeSave($dateString) {
+      return DateTime::createFromFormat('d/m/Y', $dateString)->format('Y-m-d');
+    }
+
+    public function alpha($check) {
+      $value = array_values($check);
+      $value = $value[0];
+      return preg_match('|^[a-zA-Z ]*$|', $value);
+    }
+
 }
 
 ?>
